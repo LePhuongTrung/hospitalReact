3e1b3b0e1cacb62f8ee2c0817fefeb44ad34fa0b
LePhuongTrung <trunglp2001@gmail.com>
2022-12-15 16:27:22 +0700

    add report router

---
+++ src/manager/pages/report/ListReporst.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Pagination from "../../../public/components/pagination";
+
+function Index() {
+  return (
+    <>
+      <div className="xl:w-3/4 2xl:w-4/5 w-full">
+        <div className="bg-white px-4 md:px-10 pb-5">
+          <div className="overflow-x-auto">
+            <table className="w-full whitespace-nowrap">
+              <tbody>
+                <tr className=" border-b-2  text-sm leading-none text-gray-600 h-16">
+                  <td className="pl-8">
+                    <p class="font-bold">Room ID</p>
+                  </td>
+                  <td className="pl-16">
+                    <p class="font-bold">Faculty classification</p>
+                  </td>
+                  <td className="pl-16">
+                    <p class="font-bold">Doctor's name</p>{" "}
+                  </td>
+                  <td className="pl-16">
+                    <p class="font-bold">Current Number</p>{" "}
+                  </td>
+                  <td className="pl-16">
+                    <p class="font-bold">Total Number</p>{" "}
+                  </td>
+                  <td className="pl-8">
+                    <p class="font-bold">Priority room</p>{" "}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="ml-418 mt-6">
+            <Pagination />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Index;
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Manager from "./manager/pages/Manager";
+import Report from "./manager/pages/report/ListReporst";
 import AddRoom from "./manager/pages/room/AddRoom";
 import ListRoom from "./manager/pages/room/ListRoom";
 import Room from "./manager/pages/room/Room";
 
+import VerificationSuccess from "./public/pages/auth/verificationSuccess";
 import ERROR from "./public/pages/Error/404";
 import ERROR1 from "./public/pages/Error/noPermission";
 
 import Login from "./public/pages/auth/Login";
 import Signup from "./public/pages/auth/SignUp";
-import VerificationSuccess from "./public/pages/auth/verificationSuccess";
+
 import User from "./user/pages/User";
 
 import AuthenticationRoutes from "./middleware/authentication";
@@ -29,6 +31,7 @@ const AppRouter = () => (
             <Route path="AddRoom" element={<AddRoom />} />
             <Route path="ListRoom" element={<ListRoom />} />
           </Route>
+          <Route path="Report" element={<Report />}></Route>
         </Route>
       </Route>
       <Route path="SignUp" element={<Signup />} />
