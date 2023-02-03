a28bb86a0adfc8912018b6a9ebbe36bf034221f7
LePhuongTrung <trunglp2001@gmail.com>
2023-02-03 18:22:57 +0700

    No done handle data doctor

--- src/public/pages/auth/Login.js
+++ src/public/pages/auth/Login.js
@@ -156,7 +156,6 @@ export default function Login() {
               <div>
                 <button
                   type="submit"
-                  // onClick={handleLogin}
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Sign in
