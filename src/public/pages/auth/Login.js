7e2cc87cef5350f61ed542edc2f7bd1f129c0815
LePhuongTrung <trunglp2001@gmail.com>
2023-03-23 17:59:02 +0700

    add eye to input password

--- src/public/pages/auth/Login.js
+++ src/public/pages/auth/Login.js
@@ -10,8 +10,8 @@ import { setLoggedInUser } from "../../../redux/auth/AuthStatus";
 
 import { login } from "../../../public/api/auth";
 
+import { useEffect, useState } from "react";
 import CustomInput from "../../components/CustomInput";
-import InputPasswordComponents from "../../components/InputPassword";
 
 const schemaValidation = yup
   .object()
@@ -24,7 +24,17 @@ const schemaValidation = yup
 export default function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const [inputType, setInputType] = useState("password");
 
+  // handle toggle
+  const toggle = () => {
+    setOpen(!open);
+  };
+  useEffect(() => {
+    console.log("🚀 ~ file: InputPassword.js:12 ~ toggle ~ open:", open);
+    setInputType(open ? "text" : "password");
+  }, [open]);
   const {
     register,
     handleSubmit,
@@ -94,13 +104,67 @@ export default function Login() {
                 errors={errors.email}
                 {...register("email")}
               />
-              <InputPasswordComponents
-                label="Password"
-                type="password"
-                placeholder="Enter your password"
-                errors={errors.password}
-                {...register("password")}
-              />
+              <div>
+                <label
+                  htmlFor="text"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <div className="mt-1 relative">
+                  <input
+                    type={inputType}
+                    placeholder="Enter your password"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    {...register("password")}
+                  />
+
+                  {open === false ? (
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-4">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                        onClick={toggle}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                      </svg>
+                    </div>
+                  ) : (
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-4">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                        onClick={toggle}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                        />
+                      </svg>
+                    </div>
+                  )}
+
+                  {errors && <p className="text-[#FF0000]">{errors.message}</p>}
+                </div>
+              </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-4">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
